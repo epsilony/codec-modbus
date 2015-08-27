d29502292def0c9b059c8b248324a658efd40610
Man <epsilony@epsilony.net>
2015-08-27 12:31:22 +0800

    add a simple modbus master side initializer

---
+++ src/main/java/net/epsilony/utils/codec/modbus/SimpModbusMasterChannelInitializer.java
@@ -0,0 +1,69 @@
+/*
+ *
+ * The MIT License (MIT)
+ * 
+ * Copyright (C) 2013 Man YUAN <epsilon@epsilony.net>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package net.epsilony.utils.codec.modbus;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import net.epsilony.utils.codec.modbus.reqres.ModbusRequest;
+
+/**
+ * @author <a href="mailto:epsilony@epsilony.net">Man YUAN</a>
+ *
+ */
+public class SimpModbusMasterChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    public static long DEFAULT_REQUEST_LIFETIME = 5000;
+    private Map<Integer, ModbusRequest> requestRecorder = new LinkedHashMap<>();
+    private long requestLifeTime = DEFAULT_REQUEST_LIFETIME;
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ch.pipeline().addLast(new ModbusMasterCodec(requestRecorder::remove, (res) -> {
+            requestRecorder.put(res.getTransectionId(), res);
+            if (requestLifeTime <= 0) {
+                return;
+            }
+            ch.eventLoop().schedule(() -> {
+                requestRecorder.remove(res.getTransectionId());
+            } , requestLifeTime, TimeUnit.MILLISECONDS);
+        }));
+    }
+
+    public long getRequestLifeTime() {
+        return requestLifeTime;
+    }
+
+    public void setRequestLifeTime(long requestLifeTime) {
+        if (requestLifeTime < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.requestLifeTime = requestLifeTime;
+    }
+
+}
